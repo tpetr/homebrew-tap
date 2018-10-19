14f920c631bbed5ebd0a8a3bada54b9e214583de
tpetr <trpetr@gmail.com>
2018-10-19 18:06:12 -0400

    Brew formula update for dns-proxy version v0.1.2

--- dns-proxy.rb
+++ dns-proxy.rb
@@ -1,9 +1,9 @@
 class DnsProxy < Formula
   desc "DNS proxy"
   homepage "https://github.com/tpetr/dns-proxy"
-  url "https://github.com/tpetr/dns-proxy/releases/download/v0.1.1/dns-proxy_0.1.1_Darwin_x86_64.tar.gz"
-  version "0.1.1"
-  sha256 "c417343c651cd2a342da5de644049ca16111f339d4def44b7f223c136b99550e"
+  url "https://github.com/tpetr/dns-proxy/releases/download/v0.1.2/dns-proxy_0.1.2_Darwin_x86_64.tar.gz"
+  version "0.1.2"
+  sha256 "b42dcf390fe6202209f7e4a7c296eec2cfd148e07d1235162c8aa50b89a6f598"
 
   def install
     bin.install "dns-proxy"
