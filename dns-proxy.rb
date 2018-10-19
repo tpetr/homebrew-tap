88ddef73c9d6ead0a0d68ccbf5fe4eb1e1b37eb5
tpetr <tpetr@hubspot.com>
2018-10-19 17:22:21 -0400

    example config + plist

--- dns-proxy.rb
+++ dns-proxy.rb
@@ -17,4 +17,50 @@ class DnsProxy < Formula
       prefix.install_metafiles
     end
   end
+
+  def post_install
+    unless File.file?(etc/"dns-proxy.json")
+      (etc/"dns-proxy.json").write(<<~EOS
+      {
+        "ListenAddr": ":10053",
+        "Handlers": [
+          {
+            "URL": "https://cloudflare-dns.com/dns-query"
+          },
+          {
+            "URL": "udp://1.1.1.1:53"
+          }
+        ]
+      }
+      EOS
+      )
+    end
+  end
+
+  def caveats; <<~EOS
+    To configure dns-proxy, take the default example configuration at
+      #{etc}/dns-proxy.json and edit to taste.
+  EOS
+  end
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{bin}/dns-proxy</string>
+          <string>#{etc}/dns-proxy.json</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+    </plist>
+  EOS
+  end
 end
