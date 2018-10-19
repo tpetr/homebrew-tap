0af587a1f97e1923c16e4df1ab98d012353fa9ad
Tom Petr <tpetr@hubspot.com>
2018-10-19 17:02:09 -0400

    add sha

--- dns-proxy.rb
+++ dns-proxy.rb
@@ -2,6 +2,7 @@ class DnsProxy < Formula
   desc "DNS Proxy"
   homepage "https://github.com/tpetr/dns-proxy"
   url "https://github.com/tpetr/dns-proxy/archive/v0.1.0.tar.gz"
+  sha256 "32e10f5031275bdf9399b0d306f42f9a8baafda4ed806cc271160cbba74dfd39"
 
   depends_on "dep" => :build
   depends_on "go" => :build
