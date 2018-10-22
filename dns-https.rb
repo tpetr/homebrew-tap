2f04eb1149584b73104912c432ca13eefe0b9235
tpetr <trpetr@gmail.com>
2018-10-22 13:02:44 -0400

    Brew formula update for dns-https version v0.1.1

--- dns-https.rb
+++ dns-https.rb
@@ -3,7 +3,7 @@ class DnsHttps < Formula
   homepage "https://github.com/tpetr/dns-https"
   url "https://github.com/tpetr/dns-https/releases/download/v0.1.1/dns-https_0.1.1_Darwin_x86_64.tar.gz"
   version "0.1.1"
-  sha256 "783e7a813e86f7db11a3183f7126d7a42a1759ca45f784efa4dd7bb8ebff1ccc"
+  sha256 "11ea587e29e88ad3bbff7ca172a6bb6704a5cdd04683158db7cdb6679cc04ba4"
 
   def install
     bin.install "dns-https"
